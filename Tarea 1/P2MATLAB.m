e5c87aab43a8568bb0106cdd2762b92f6524a8a6
Alexis Contreras R <ale.aicr@gmail.com>
2022-04-01 22:24:40 -0300

    Fix

--- Tarea 1/P2MATLAB.m
+++ Tarea 1/P2MATLAB.m
@@ -2,14 +2,16 @@
 clear variables
 close all
 clc
-
+disp('<a href="https://github.com/aleaicr/Tareas_Sismica">Github - Tareas_Sismica - Alexis Contreas R.</a>')
+disp('Si no corre el registro de Pica es por que hay que quitar los comentarios')
+disp('LINEA 22 - LINEA 32')
 %% Importar datos
 % Todos los datos tienen paso temporal de 0.005
 % Todos tienen unidad cm/s2 para la aceleración del registro excepto pica2005_ew.txt
 
-Reg = readmatrix("Concepcion2010-L.txt");
-Nombre = "Concepción"
-mc = length(Reg);
+Regi = readmatrix("Concepcion2010-L.txt");
+Nombre = "Concepción";
+mc = length(Regi);
 
 %% Vector de tiempo;
 
@@ -33,23 +35,25 @@ t_reg = (0:dt:(mc-1)*dt).'; % Vector que contiene los tiempos
 %% Generación de Gráficos
 
 figure
-plot(t_reg,reg)
+plot(t_reg,Regi)
 xlabel("Tiempo [s]")
 ylabel("Aceleración [cm/s^2]")
+title(Nombre)
+grid on
 
 
 %% Calcular PGA
-PGA_reg = max(Reg);
+PGA_reg = max(Regi);
 
 %% Maximos Sostenidos
 
 maximos_sostenidos = []; % Se genera una lista vacía
-max_array = [Reg(1,1)]; % Valor inicial de aceleración en el registro
+max_array = [Regi(1,1)]; % Valor inicial de aceleración en el registro
 j = 1;   % Inicio en el tramo 1
 signo = [];
 
 for i = 2:mc
-    if sign(Reg(i,1)*Reg(i-1,1)) == -1   %Si es negativo entonces hay un nuevo tramo
+    if sign(Regi(i,1)*Regi(i-1,1)) == -1   %Si es negativo entonces hay un nuevo tramo
         % maximos_sostenidos = [maximos_sostenidos; max];
         maximos_sostenidos(j,1) = abs(max_array(j,1));
         maximos_sostenidos(j,2) = signo(j,1);
@@ -61,25 +65,26 @@ for i = 2:mc
         max_array(j,1) = 0;
         clear p % No es de interés guardar la variable p
     end
-    if abs(Reg(i,1)) > abs(max_array(j,1))
-        max_array(j,1) = Reg(i,1);
-        signo(j,1) = sign(Reg(i,1));
+    if abs(Regi(i,1)) > abs(max_array(j,1))
+        max_array(j,1) = Regi(i,1);
+        signo(j,1) = sign(Regi(i,1));
     end
 end
 
 % Ordenar las aceleraciones sostenidas en orden descendente (la primera es la mayor aceleración)
-sortrows(maximos_sostenidos,'descend')
+
+max_ordenados = sortrows(maximos_sostenidos,'descend');
+max_sostenidos_ordenados = [];
 
 for i = 1:j-1
-    var = maximos_sostenidos(i,1);
-    maximos_sostenidos(i,1) = var*maximos_sostenidos(i,2);
+    maximos_sostenidos_ordenados(i,1) = max_ordenados(i,1)*maximos_sostenidos(i,2);
 end
 
 %Mostrar las aceleraciones sostenidas
-disp(maximos_sostenidos(3,1))
-disp(maximos_sostenidos(5,1))
-disp(maximos_sostenidos(7,1))
-disp(maximos_sostenidos(9,1))
+sprintf('La 3ra aceleracion máxima sostenida es %d cm/s2',maximos_sostenidos_ordenados(3,1))
+sprintf('La 5ta aceleracion máxima sostenida es %d cm/s2',maximos_sostenidos_ordenados(5,1))
+sprintf('La 7ma aceleracion máxima sostenida es %d cm/s2',maximos_sostenidos_ordenados(7,1))
+sprintf('La 9na aceleracion máxima sostenida es %d cm/s2',maximos_sostenidos_ordenados(9,1))
 
 %% Duración del movimiento fuerte
 g = 9.81; %m/s^2
@@ -87,33 +92,30 @@ u = 0.05; % porcentaje del máximo admitido
 umbral = u*g;
 
 for i = 1:mc
-    if abs(Reg(i,1)) > umbral
+    if abs(Regi(i,1)) > umbral
         ti = t_reg(i,1);      % guardamos la primera vez que supera ese umbral
         break %paramos para que no 
     end
 end
 for i = 1:mc
-    if abs(Reg(i,1)) > umbral
+    if abs(Regi(i,1)) > umbral
         tf = t_reg(i,1);  % guardamos la última vez que se supera ese umbral
     end
 end
 dur = tf - ti; % Duración del movimiento fuerte
-
+sprintf('La duración del movimiento fuerte es de %d [sec]',dur)
 %% Duración con Método 5% a 95% de Intensidad de Arias
 
 % Generamos la "gráfica" de la Integral de Arias I_A
 
-Ia = [];
-Ia_new = 0; 
+Ia(1,1) = 0;
 
-for i = 1:mc
-    new_add = pi/(2*g)*abs(Reg(i,1))^2*dt;
-    p = Ia_new;
-    Ia_new = p + new_add;
-    clear p
-    Ia = [Ia; Ia_new];
+for i = 2:mc+1
+    new_add = pi/(2*g)*abs(Regi(i-1,1))^2*dt;
+    Ia(i,1) = Ia(i-1,1) + new_add;
 end
-clear Ia_new
+
+clear new_add p
 
 % Máximo valor de la Integral de Arias
 
@@ -130,4 +132,5 @@ for i = 1:mc
     end
 end
 
-dur_arias = tf - ti; % Duración con Método de La Integral de Arias
+dur_arias = tf - ti; % Duración con Método de la Intensidad de Arias
+sprintf('La duración con el método de la Intensidad de Arias es %d [sec]',dur_arias)
