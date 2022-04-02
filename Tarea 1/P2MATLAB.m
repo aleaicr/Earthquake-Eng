e6ba9ecaebda640d0041ef7dc78691c6a4a43503
Alexis Contreras R. <ale.aicr@gmail.com>
2022-04-01 21:54:08 -0300

    Actualización

--- Tarea 1/P2MATLAB.m
+++ Tarea 1/P2MATLAB.m
@@ -7,87 +7,73 @@ clc
 % Todos los datos tienen paso temporal de 0.005
 % Todos tienen unidad cm/s2 para la aceleración del registro excepto pica2005_ew.txt
 
-Concepcion = readmatrix("Concepcion2010-L.txt");
-mc = length(Concepcion);
-
-Santiago = readmatrix("stgocentro2010-L.txt");
-ms = length(Santiago);
-
-Talca = readmatrix("Talca2010-L.txt");
-mt = length(Talca);
-
-Valparaiso = readmatrix("valparaisoUTFSM2010-L.txt");
-mv = length(Valparaiso);
-
-Pica = readmatrix("pica2005_ew.txt");
-mp = length(Pica);
-
-Northridge = readmatrix("Northridge-Tarzana-EW.txt"); 
-mn = length(Northridge);
+Reg = readmatrix("Concepcion2010-L.txt");
+Nombre = "Concepción"
+mc = length(Reg);
 
 %% Vector de tiempo;
 
-dt = 0.005; % general para todos
-tc = 0:dt:(mc-1)*dt; %Vector de tiempos para Concepción
-ts = 0:dt:(ms-1)*dt; % Vector de tiempos para Santiago
-tt = 0:dt:(mt-1)*dt; % Vector de tiempos para TAlca
-tv = 0:dt:(mv-1)*dt; % Vector de tiempos para valparaíso
-tp = 0:dt:(mp-1)*dt; % Vector de tiempos para Pica
-tn = 0:dt:(mn-1)*dt; % Vector de tiempos apra Northridge - Tarzana
+dt = 0.005; % Paso temporal de la toma de datos
+t_reg = (0:dt:(mc-1)*dt).'; % Vector que contiene los tiempos
 
-%% Conversión a cm/s2 para Pica
 
-Pica_cms2 = Pica*981;  % g = 9.81 m/s2 = 981 cm/s2
+%% Ordenar Pica (registro viene con muchos NaN)
+% QUITAR COMENTARIO SI SE ESTÁ VIENDO EL REGISTRO DE PICA
+% Pica2 = [];
+% for i = 1:mp
+%     if Pica(i,3) > 0
+%         Pica2 = Pica(i,1);
+%     end
+%     if Pica(i,2) < 0
+%         Pica2 = Pica(i,1);
+%     end
+% end
+% Pica = Pica2*981;  % g = 9.81 m/s2 = 981 cm/s2
 
 %% Generación de Gráficos
 
 figure
-plot(tc,Concepcion);
-figure
-plot(ts,Santiago);
-figure
-plot(tt, Talca);
-figure
-plot(tv, Valparaiso);
-figure
-plot(tp, Pica_cms2);
-figure
-plot(tn, Northridge);
+plot(t_reg,reg)
+xlabel("Tiempo [s]")
+ylabel("Aceleración [cm/s^2]")
+
 
 %% Calcular PGA
-PGAc = max(Concepcion);
-PGAs = max(Santiago);
-PGAt = max(Talca);
-PGAv = max(Valparaiso);
-PGAp = max(Pica_cms2);
-PGAn = max(northridge);
+PGA_reg = max(Reg);
 
 %% Maximos Sostenidos
-% Para concepción
 
 maximos_sostenidos = []; % Se genera una lista vacía
-max = Concepcion(1,1); % Valor inicial de aceleración en el registro
-j = 1;   %inicio en el tramo 1
+max_array = [Reg(1,1)]; % Valor inicial de aceleración en el registro
+j = 1;   % Inicio en el tramo 1
+signo = [];
 
 for i = 2:mc
-    if sign(Concepcion(i,1))*Concepcion(i-1,1)) == -1   %Si es negativo entonces hay un nuevo tramo
+    if sign(Reg(i,1)*Reg(i-1,1)) == -1   %Si es negativo entonces hay un nuevo tramo
         % maximos_sostenidos = [maximos_sostenidos; max];
-        maximos_sostenidos(j,1) = max;
+        maximos_sostenidos(j,1) = abs(max_array(j,1));
+        maximos_sostenidos(j,2) = signo(j,1);
         % if maximos_sostenidos(j,1) < max || isempty(maximos_sostenidos)
         %     maximos_sostenidos = [lista_maximos sostenidos; last_max];
         % end
-        max = 0;
         p = j;
         j = p + 1;    % Siguiente tramo (no me funciona j = j + 1;)
+        max_array(j,1) = 0;
         clear p % No es de interés guardar la variable p
     end
-    if abs(Concepcion(i,1)) > abs(max)
-        max = Concepcion(i,1);
+    if abs(Reg(i,1)) > abs(max_array(j,1))
+        max_array(j,1) = Reg(i,1);
+        signo(j,1) = sign(Reg(i,1));
     end
 end
 
 % Ordenar las aceleraciones sostenidas en orden descendente (la primera es la mayor aceleración)
-sort(maximos_sostenidos,'descend');
+sortrows(maximos_sostenidos,'descend')
+
+for i = 1:j-1
+    var = maximos_sostenidos(i,1);
+    maximos_sostenidos(i,1) = var*maximos_sostenidos(i,2);
+end
 
 %Mostrar las aceleraciones sostenidas
 disp(maximos_sostenidos(3,1))
@@ -97,46 +83,51 @@ disp(maximos_sostenidos(9,1))
 
 %% Duración del movimiento fuerte
 g = 9.81; %m/s^2
-umbral = 0.05*g;
+u = 0.05; % porcentaje del máximo admitido
+umbral = u*g;
 
 for i = 1:mc
-    if abs(Concepcion(i,1)) > umbral
-        ti = tc(i,1);      % guardamos la primera vez que supera ese umbral
-        break
+    if abs(Reg(i,1)) > umbral
+        ti = t_reg(i,1);      % guardamos la primera vez que supera ese umbral
+        break %paramos para que no 
     end
 end
-for i = mc:1 % me voy devolviendo por la lista
-    if abs(Concepcion(i,1)) > umbral
-        tf = tc(i,1);    % guardamos la última vez que se supera ese umbral
-        break
+for i = 1:mc
+    if abs(Reg(i,1)) > umbral
+        tf = t_reg(i,1);  % guardamos la última vez que se supera ese umbral
     end
 end
-dur = tf-ti;
+dur = tf - ti; % Duración del movimiento fuerte
 
 %% Duración con Método 5% a 95% de Intensidad de Arias
 
 % Generamos la "gráfica" de la Integral de Arias I_A
 
 Ia = [];
+Ia_new = 0; 
 
 for i = 1:mc
-    new_add = pi*/(2*g)abs(Concepcion(i,1))^2*dt;
-    Ia_new = Ia_new + new_add;
+    new_add = pi/(2*g)*abs(Reg(i,1))^2*dt;
+    p = Ia_new;
+    Ia_new = p + new_add;
+    clear p
     Ia = [Ia; Ia_new];
 end
-% Máxima Ia (Para saber el porcentaje del total)
+clear Ia_new
 
-Iamax = max(Ia);
+% Máximo valor de la Integral de Arias
+
+Iamax = max(Ia); % Para saber que valor es 5 y 95 % del total
 
 % Determinamos cuando se genera el 5 y 95 por ciento
 
 for i = 1:mc
     if Ia(i,1) > 0.05*Iamax
-        ti = tc(i,1);
+        ti = t_reg(i,1);
     end
     if Ia(i,1) > 0.95*Iamax
-        tf = tc(i,1);
+        tf = t_reg(i,1);
     end
 end
 
-dur_mov_fuerte = tf - ti;
+dur_arias = tf - ti;
