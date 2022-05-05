5f5fcc4006ee9092e4247d2d32ed1551fda72f0d
Alexis Contreras R <ale.aicr@gmail.com>
2022-05-05 11:32:03 -0400

    Update

---
+++ Tarea 3 sismica/Tarea_3.m
@@ -0,0 +1,19 @@
+%% Tarea 1
+% Cristobal Adasme
+%
+% Alexis Contreras
+
+%% Inicializar
+clear all
+close all
+clc
+
+%% P1.1
+
+%% Importar registro
+
+registro = readmatrix("Northridge-Tarzana-EW".txt);
+
+%%
+
+
